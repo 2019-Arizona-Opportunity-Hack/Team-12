0b2062d50cf9accc7786bfb622fa0168f92d3be7
Nina Tran <ninatran@Ninas-MacBook-Air.local>
2019-10-19 13:50:01 -0700

    added hotels route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Link } from "react-router-dom";
 import Cabs from "./components/cabs/index";
+import Hotels from "./components/hotels/index";
 
 function App() {
   return (
@@ -10,8 +11,12 @@ function App() {
         <li>
           <Link to="/cabsPage">Go to cabs page</Link>
         </li>
+        <li>
+          <Link to="/hotelsPage">Go to hotels page</Link>
+        </li>
       </ul>
       <Route exact path="/cabsPage" component={Cabs}/>
+      <Route exact path="/hotelsPage" component={Hotels}/>
     </div>
   );
 }
---
+++ frontend/src/components/hotels/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App(){
+    return (
+        <div>
+            Hotels
+        </div>
+    )
+}
+
+export default App;
