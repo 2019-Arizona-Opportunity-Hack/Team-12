727fcfbcb3a784d086d41fd2073d2364df8c2e5b
Nick42791 <nick42791@gmail.com>
2019-10-19 13:25:12 -0700

    Gutted Front end and added cors to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from "react-router-dom";
+import Cabs from "./components/cabs/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/cabsPage">Go to cabs page</Link>
+        </li>
+      </ul>
+      <Route exact path="/cabsPage" component={Cabs}/>
     </div>
   );
 }
