99a83cdcc5cb3a9fde5bd9b480d83d72d7643f4c
Nina Tran <ninatran@Ninas-MacBook-Air.local>
2019-10-19 20:31:10 -0700

    bootstrap added

--- frontend/src/components/homepage/RequestForm.js
+++ frontend/src/components/homepage/RequestForm.js
@@ -6,19 +6,19 @@ import ServiceOption from './ServiceOption.js'
 
 const RequestForm = ({updateService, updateGender, changeHandler}) => {
     return(
-        <div className="container">
+        <div className="containerx">
             <form autocomplete="off">
-                <div className="field">
+                <div className="form-group">
                     <label>Name </label>
                     <input type="text" id="userName" name="name"
                         onChange={changeHandler}/>
                 </div>
-                <div className="field">
+                <div className="form-group">
                     <label>Address</label>
                     <input type="text" id="currentLocation" name="address"
                         onChange={changeHandler}/>
                 </div>
-                <div className="field">
+                <div className="form-group">
                     <label>Gender</label>
                     <input type="radio" value="female" name="gender"
                         onChange={updateGender} />
@@ -27,23 +27,23 @@ const RequestForm = ({updateService, updateGender, changeHandler}) => {
                         onChange={updateGender} />
                     <label for="male">Male</label>
                 </div>
-                <div className="field">
+                <div className="form-group">
                     <label>Age</label>
                     <input type="number" name="age" min="0" max="100"
                         onChange={changeHandler}/>
                 </div>
-                <div className="field">
+                <div className="form-group">
                     <label>Are you traveling alone?</label>
                     <input type="radio" id="alone" name="numPeople"/>
                     <label for="alone">Yes</label>
                     <input type="radio" id="morePeople" name="numPeople" />
                     <label for="morePeople">No</label>
                 </div>
-                <div className="field">
+                <div className="form-group">
                     <label> How many additional people will you be travelling with (not including yourself)</label>
                     <input type="number" name="numPeople" min="1" max="10"/>
                 </div>
-                <div className="field">
+                <div className="form-group">
                     <label>Are you traveling with pets?</label>
                     <input type="radio" id="withPet" name="hasPets" />
                     <label for="withPet">Yes</label>
