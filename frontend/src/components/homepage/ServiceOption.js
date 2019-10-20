e2ad906db2cc4a761e09a99a339dcd9aceac8f96
Nina Tran <ninatran@Ninas-MacBook-Air.local>
2019-10-20 13:39:20 -0700

    added icons

--- frontend/src/components/homepage/ServiceOption.js
+++ frontend/src/components/homepage/ServiceOption.js
@@ -1,19 +1,34 @@
 import React from "react";
 import './style.css';
+import CabLogo from './cab.png';
+import HotelLogo from './room.png';
+import Both from './both.png';
 
 const ServiceOption =({updateService})=>{
 
         return(
             <div className="selectService">
 
-                <label  htmlFor="selectCab"><input type="radio" id="selectCab" name="typeOfService" value="cab"
-                    onChange={updateService}/>Ride</label>
+                <label  htmlFor="selectCab">
+                <input type="radio" id="selectCab" name="typeOfService" value="cab"
+                    onChange={updateService}/>
+                    <img src={CabLogo} alt="cab icon" width="100px"/>
+                    <div>Ride</div>
+                </label>
 
-                <label  htmlFor="selectHotel"><input type="radio" id="selectHotel" name="typeOfService"
-                    onChange={updateService} value="hotel"/>Roof</label>
+                <label  htmlFor="selectHotel">
+                    <input type="radio" id="selectHotel" name="typeOfService"
+                    onChange={updateService} value="hotel"/>
+                    <img src={HotelLogo} alt="hotel icon" width="100px"/>
+                    <div>Roof</div>
+                </label>
 
-                <label  htmlFor="selectBoth"><input type="radio" id="selectBoth" name="typeOfService"
-                    onChange={updateService} value="both" />Both</label>
+                <label  htmlFor="selectBoth">
+                    <input type="radio" id="selectBoth" name="typeOfService"
+                    onChange={updateService} value="both" />
+                    <img src={Both} alt="hotel and cab icon" width="100px"/>
+                    <div>Both</div>
+                </label>
 
             </div>
         );
